bfb37401f8bb0728343d323ef317ecfe3b223663
khanh <vckhanhitiu1@gmail.com>
2019-06-16 20:26:31 +0700

    modify code

--- src/views/Home/MainPage.js
+++ src/views/Home/MainPage.js
@@ -1,37 +1,9 @@
 import React, { Component } from 'react'
-import { Modal, Button, Icon } from 'antd';
 import 'antd/dist/antd.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
-import MiniCart from '../../components/MiniCart'
 import TopBar from '../../components/TopBar'
-import ViewBasket from '../Basket/ViewBasket'
-import PopularScreen from '../../components/screen/PopularScreen';
-import PromotionScreen from '../../components/screen/PromotionScreen';
 import { BrowserRouter as Router} from "react-router-dom";
 
 export default class MainPage extends Component {
-    state = { visible: false };
-
-  showModal = () => {
-    this.setState({
-      visible: true,
-    });
-  };
-
-  handleOk = e => {
-    console.log(e);
-    this.setState({
-      visible: false,
-    });
-  };
-
-  handleCancel = e => {
-    console.log(e);
-    this.setState({
-      visible: false,
-    });
-  };
     render() {
         return (
             <div className="container-fluid w-100 p-0">
@@ -40,32 +12,6 @@ export default class MainPage extends Component {
                         <TopBar/>
                     </Router>
                 </div>
-                <div className="col-lg-12 py-1">
-                    <div className="d-flex">
-                        <div className="" style={{width:"85%"}}>
-                            <PromotionScreen/>
-                        </div>
-                        <div className="ml-4"  style={{width:"15%"}}>
-                            <Button type="primary" onClick={this.showModal}>
-                                <Icon type="left" />
-                            </Button>
-                            <MiniCart/>
-                        </div>
-                    </div>
-                </div>
-                <Modal
-                    title={<span><FontAwesomeIcon icon={faShoppingCart} /> View Basket</span> }
-                    visible={this.state.visible}
-                    onOk={this.handleOk}
-                    onCancel={this.handleCancel}
-                    closable="true"
-                    width="80%"
-                    bodyStyle={{padding:0}}
-                    footer={null}
-                    centered
-                    >
-                    <ViewBasket/>
-                </Modal>
             </div>
 
 
